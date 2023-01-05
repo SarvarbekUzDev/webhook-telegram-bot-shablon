f5b6df7ad0a71b878daddc3c2424349e2116da10
Sarvarbek <120723170+SarvarbekUzDev@users.noreply.github.com>
2023-01-05 18:14:17 +0300

    Update inline_keyboards.py

--- webhook-telegram-bot-shablon/keyboards/inline_keyboards.py
+++ webhook-telegram-bot-shablon/keyboards/inline_keyboards.py
@@ -4,4 +4,4 @@ from dp.keyboards.inline_keyboard import InlineKeyboardButton, InlineKeyboardMar
 
 like = InlineKeyboardButton(text="Like👍", callback_data="like")
 mysite = InlineKeyboardButton(text="Your my website🚥", url="https://github.com/SarvarbekUzDev")
-inline_markup = InlineKeyboardMarkup().add(like, mysite)
+inline_markup = InlineKeyboardMarkup(variable_name="inline_markup").add(like, mysite)
