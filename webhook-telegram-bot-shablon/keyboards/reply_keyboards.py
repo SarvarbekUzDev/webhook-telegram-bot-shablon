89c1d4a36e597337ad84e0492acae3bfcf21e298
Sarvarbek <120723170+SarvarbekUzDev@users.noreply.github.com>
2023-01-05 18:14:54 +0300

    Update reply_keyboards.py

--- webhook-telegram-bot-shablon/keyboards/reply_keyboards.py
+++ webhook-telegram-bot-shablon/keyboards/reply_keyboards.py
@@ -3,4 +3,4 @@ from dp.keyboards.reply_keyboard import ReplyKeyboardButton, ReplyKeyboardMarkup
 
 button1 = ReplyKeyboardButton(text="State🚥")
 button2 = ReplyKeyboardButton(text="Inline Keyboard⌨️")
-btn_markup = ReplyKeyboardMarkup(resize_keyboard=True).add(button1, button2)
+btn_markup = ReplyKeyboardMarkup(resize_keyboard=True, variable_name="btn_markup").add(button1, button2)
